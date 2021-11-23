43f057cb3674dc240887ecb024c417e96a09f238
Максим Матросов <matrosov.02@bk.ru>
2021-11-23 15:54:20 +0400

    Генерация ключей и их сериализация

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 ##Вариант 2
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives.ciphers.algorithms import Camellia
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import padding
@@ -9,15 +8,81 @@ from tqdm import tqdm
 import json
 import argparse
 import os
-import pickle
 
 settings = {
-    'initial_file': "C://Users//matro//OneDrive//Рабочий стол//Учебники//ОИБ//Лаба 3//file.txt",
-    'encrypted_file': 'C://Users//matro//OneDrive//Рабочий стол//Учебники//ОИБ//Лаба 3//encrypted_file.txt',
-    'decrypted_file': 'C://Users//matro//OneDrive//Рабочий стол//Учебники//ОИБ//Лаба 3//decrypted_file.txt',
-    'symmetric_key': 'C://Users//matro//OneDrive//Рабочий стол//Учебники//ОИБ//Лаба 3//symmetric_key.txt',
-    'public_key': 'C://Users//matro//OneDrive//Рабочий стол//Учебники//ОИБ//Лаба 3//public_key.pem',
-    'secret_key': 'C://Users//matro//OneDrive//Рабочий стол//Учебники//ОИБ//Лаба 3//secret_key.pem',
+    'initial_file': "file.txt",  # Файл с текстом
+    'encrypted_file': 'encrypted_file.txt',  # Зашифрованный текст
+    'decrypted_file': 'decrypted_file.txt',  # Дешиврованный текст
+    'symmetric_key': 'symmetric_key.txt',  # Симметричный ключ Camellia
+    'public_key': 'public_key.pem',  # Открытый ключ RSA
+    'secret_key': 'secret_key.pem',  # закрытый ключ RSA
 }
 
 
+def input_len_key() -> int:
+    """
+        Функция возвращает длину ключа для алгоритма Camellia
+    """
+    while 1:
+        os.system('CLS')
+        print("Выберите длину ключа\n"
+              "1.128\n"
+              "2. 192\n"
+              "3. 256\n"
+              "Ваш выбор:")
+        alternative = input()
+        if int(alternative) == 1:
+            return int(128)
+        if int(alternative) == 2:
+            return int(192)
+        if int(alternative) == 3:
+            return int(256)
+
+
+def generate_key(encrypted_symmetric_key: str, public_key_path: str, secret_key_path: str) -> None:
+    symmetrical_key = algorithms.Camellia(os.urandom(int(input_len_key() / 8)))
+    keys = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    private_key = keys
+    public_key = keys.public_key()
+    with open(public_key_path, 'wb') as public_out:
+        public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                 format=serialization.PublicFormat.SubjectPublicKeyInfo))
+    with open(secret_key_path, 'wb') as private_out:
+        private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                    format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                    encryption_algorithm=serialization.NoEncryption()))
+    encrypt_symmetrical_key = public_key.encrypt(symmetrical_key.key,
+                                                 padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                              algorithm=hashes.SHA256(),
+                                                              label=None))
+
+    with open(encrypted_symmetric_key, 'wb') as summetric_out:
+        summetric_out.write(encrypt_symmetrical_key)
+
+
+"""
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required = True)
+group.add_argument('-gen','--generation',help='Запускает режим генерации ключей')
+group.add_argument('-enc','--encryption',help='Запускает режим шифрования')
+group.add_argument('-dec','--decryption',help='Запускает режим дешифрования')
+"""
+
+# пишем в файл
+with open('settings.json', 'w') as fp:
+    json.dump(settings, fp)
+# читаем из файла
+with open('settings.json') as json_file:
+    json_data = json.load(json_file)
+
+generate_key(json_data['symmetric_key'], json_data['public_key'], json_data['secret_key'])
+"""args = parser.parse_args()
+if args.generation is not None:
+  # генерируем ключи
+else if args.encryption is not None:
+  # шифруем
+else:
+  # дешифруем"""
